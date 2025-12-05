e5661dadcee295de91d806982e1e53e2e9050a12
jjps09981 <axa9951130@gmail.com>
2025-12-05 14:13:24 +0800

    update back to top

--- src/components/ToastDemo.jsx
+++ src/components/ToastDemo.jsx
@@ -5,14 +5,14 @@ function ToastDemo() {
   return (
     <div className="flex flex-col gap-2 items-center">
       <button
-        onClick={() => toast.success("操作成功！")}
+        onClick={() => toast.success("操作成功！ 🎉")}
         className="  w-fit px-10 py-1 bg-[#4A7056] text-white rounded cursor-pointer"
       >
         顯示成功提示
       </button>
 
       <button
-        onClick={() => toast.error("發生錯誤！")}
+        onClick={() => toast.error("發生錯誤！ 😢 ")}
         className="w-fit px-10 py-1 bg-red-600 text-white rounded cursor-pointer"
       >
         顯示錯誤提示
